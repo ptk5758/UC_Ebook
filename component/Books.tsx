a699c76aae79aecc50b85db48c47374c6987c3f5
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-04 15:03:43 +0900

    sound play 1

--- component/Books.tsx
+++ component/Books.tsx
@@ -5,7 +5,7 @@ import { SoundObject } from "expo-av/build/Audio";
 
 interface IPage {
     imgElement : JSX.Element;
-    sound? : any;
+    soundPlay? : Promise<SoundObject>;
 }
 
 export function BookA() {    
@@ -18,35 +18,37 @@ export function BookA() {
     const story : IPage[] = [
         {
             imgElement : <Image source={require('../static/book/1/page(1).png')}/>,
-            sound : require('../static/book/1/sound/Hello.mp3')
+            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/Hello.mp3'))
         },
         {
             imgElement : <Image source={require('../static/book/1/page(2).png')}/>,
-            sound : require('../static/book/1/sound/Hello.mp3')
+            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/Hello.mp3'))
         },
         {
             imgElement : <Image source={require('../static/book/1/page(3).png')}/>,
-            sound : require('../static/book/1/sound/Hello.mp3')
+            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page3.mp3'))
         },
         {
             imgElement : <Image source={require('../static/book/1/page(4).png')}/>,
-            sound : require('../static/book/1/sound/Hello.mp3')
+            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page4.mp3'))
         },
         {
             imgElement : <Image source={require('../static/book/1/page(5).png')}/>,
-            sound : require('../static/book/1/sound/Hello.mp3')
+            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page5.mp3'))
         },
     ]      
-    const [sound, setSound] = useState<SoundObject | null>(null);
+    const [sound, setSound] = useState<SoundObject | null>();
     const [page, setPage] = useState<number>(0);
     const [now, setNow] = useState<IPage>(story[page]);
     function nextPage() {        
         setNow(story[page+1]);
-        setPage(page + 1);        
-        Audio.Sound.createAsync(require('../static/book/1/sound/Hello.mp3'))
+        setPage(page + 1);
+        now.soundPlay ? now.soundPlay
         .then(obj => {
-            setSound(obj); 
-        })        
+            obj.sound.playAsync();
+            setSound(obj);
+        })
+        : () => {setSound(null)};
     }   
     
     return(
