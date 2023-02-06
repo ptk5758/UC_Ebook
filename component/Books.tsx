9b06dd1e23c52b220d1d02d2f2db38669d801ec5
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-06 16:57:58 +0900

    sound issue hotfix 3

--- component/Books.tsx
+++ component/Books.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { View, Text, TouchableHighlight, Image, StyleSheet } from "react-native";
 import { Audio } from 'expo-av';
 import { SoundObject } from "expo-av/build/Audio";
 
 interface IPage {
     imgElement : JSX.Element;
-    soundPlay? : Promise<SoundObject>;
+    sound? : any;
 }
 
 export function BookA() {
@@ -25,7 +25,7 @@ export function BookA() {
     const story : IPage[] = [
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(1).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/subject.mp3'))
+            sound : require('../static/book/1/sound/subject.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(2).png')}/>,
@@ -35,136 +35,122 @@ export function BookA() {
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(4).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page4.mp3'))
+            sound : require('../static/book/1/sound/page4.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(5).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page5.mp3'))
+            sound : require('../static/book/1/sound/page5.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(6).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page6.mp3'))
+            sound : require('../static/book/1/sound/page6.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(7).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(8).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page8.mp3'))
+            sound : require('../static/book/1/sound/page8.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(9).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(10).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page10.mp3'))
+            sound : require('../static/book/1/sound/page10.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(11).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(12).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page12.mp3'))
+            sound : require('../static/book/1/sound/page12.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(13).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(14).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page14.mp3'))
+            sound : require('../static/book/1/sound/page14.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(15).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page15.mp3'))
+            sound : require('../static/book/1/sound/page15.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(16).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(17).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page17.mp3'))
+            sound : require('../static/book/1/sound/page17.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(18).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page18.mp3'))
+            sound : require('../static/book/1/sound/page18.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(19).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(20).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page20.mp3'))
+            sound :require('../static/book/1/sound/page20.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(21).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(22).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page22.mp3'))
+            sound : require('../static/book/1/sound/page22.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(23).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(24).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page24.mp3'))
+            sound : require('../static/book/1/sound/page24.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(25).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page25.mp3'))
+            sound : require('../static/book/1/sound/page25.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(26).png')}/>,
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(27).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page27.mp3'))
+            sound : require('../static/book/1/sound/page27.mp3')
         },
         {
             imgElement : <Image style={style.image} source={require('../static/book/1/page(28).png')}/>,
-            soundPlay : Audio.Sound.createAsync(require('../static/book/1/sound/page28.mp3'))
+            sound : require('../static/book/1/sound/page28.mp3')
         }
-    ]    
-      
-    const [sound, setSound] = useState<SoundObject | null>();
+    ]          
+    
     const [page, setPage] = useState<number>(0);
-    const [now, setNow] = useState<IPage>(story[page]);
-    function nextPage() {        
-        setNow({...story[page+1]});
-        const temp = {...story[page+1]}
-        temp.soundPlay ? temp.soundPlay
-        .then(obj => {
-            if(sound?.status.isLoaded) {
-                console.log(sound?.status);
-                sound.sound.unloadAsync()
-                .then(()=>{
-                    obj.sound.playAsync()
-                    .catch(err => console.log(err))            
-                    setSound(obj);
-                })
+    const [now, setNow] = useState<IPage>({...story[page]});
+    const player = useRef<Audio.Sound | null>(null);
+    const playNext = async () => {
+        try {
+            if(player.current) {
+                await player.current.unloadAsync();
             }            
-        })        
-        : () => {
-            sound && sound.sound.unloadAsync()
-        };
-        setPage(page + 1);
-    }   
-
-    // useEffect(()=>{
-    //     return sound ? () => {
-    //         sound.sound.unloadAsync()
-    //         .catch(err => console.log(err))
-    //     }   
-    //     : undefined     
-    // }, [sound]);
-
-    ////////////////
-    now.soundPlay ? now.soundPlay
-    .then(obj => {
-        obj.sound.playAsync();
-        setSound(obj);
-    })
-    : () => {setSound(null)};
+            const source = story[page + 1].sound;
+            if(!source) return;
+            const status = {shouldPlay : true};
+            const playbackInstance = new Audio.Sound();
+            await playbackInstance.loadAsync(source, status, false);
+            player.current = playbackInstance;
+            await playbackInstance.playAsync();
+            // playbackInstance.setOnPlaybackStatusUpdate(()=>{})
+        } catch (error) {
+            console.log(error);
+        }
+    }    
+    const nextPage = () => {           
+        setNow({...story[page + 1]});        
+        playNext();
+        setPage(page+1)
+    }
     
     return(
         <View>
